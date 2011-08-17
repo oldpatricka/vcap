d814c0855b67e07c1edbc5bf17bb604a101a2a30
Patrick Armstrong <patricka@uvic.ca>
2011-08-17 14:45:24 -0700

    Add optional profile.sh file to ease deployment

--- dev_setup/cookbooks/essentials/recipes/default.rb
+++ dev_setup/cookbooks/essentials/recipes/default.rb
@@ -28,3 +28,11 @@ directory File.join(node[:deployment][:config_path], "staging") do
   recursive true
   action :create
 end
+
+if node[:deployment][:profile]
+  file node[:deployment][:profile] do
+    owner node[:deployment][:user]
+    group node[:deployment][:group]
+    content "export PATH=#{node[:ruby][:path]}/bin:`#{node[:ruby][:path]}/bin/gem env gempath`/bin:$PATH"
+  end
+end
